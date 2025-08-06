771b4d4655d85c2200d3aea0b9ff7d5e3cb58b68
anaky94 <anaky.44@gmail.com>
2025-08-06 14:07:43 +0200

    air

--- Epreuve_Air/air_01.js
+++ Epreuve_Air/air_01.js
@@ -7,7 +7,6 @@ ma_fonction(string_à_couper, string_séparateur) { // syntaxe selon votre langa
 	return (tableau)
 }
 
-
 Exemples d’utilisation :
 $> python exo.py “Bonjour les gars”
 Bonjour
@@ -17,4 +16,25 @@ gars
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 */
+
+//PSEUDO CODE 
+//pour bien comprendre l''epreuve me demande de créer un programme qui decoupe les chaine de caractère en tableau [] apres le tableau ou quand il ya l'espace ou une tabulation ou un retour a la ligne je split suivre l'exemple donné 
+// initialiser un tableau vide c'est de créer une variable vide d 'un tableau 
+//Créer une boucle en pacourant lettre par lettre 
+//identifier le séparateur dans notre cas ici ("," " espace" " tabulation")
+
+ma_function (string_à_couper, string_séparateur)
+{
+	//syntaxe 
+	//#Algorithme 
+	return (tableau)
+}
+
+
+let string = "bonjour je suis fox et toi ? ";
+const strwords = string.split();
+console.log(strwords);
+
+
+
 
