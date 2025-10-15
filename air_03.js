dce6973a3c3a6657f2e8bcce33ecb7af8d2f120e
anaky94 <anaky.44@gmail.com>
2025-10-15 10:17:52 +0200

    air

--- air_03.js
+++ air_03.js
@@ -9,13 +9,18 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
  */
 
 
+
+//Utilitises
 function searchTruder(nombres) {
+    console.log("1. Nombres reçus", nombres)
     
     let compteur = {} 
-
+    console.log("2. compteur initial:", compteur)
+    
     for (let i = 0; i < nombres.length; i++ ) {
         let nombreActuel = nombres[i]
-        
+        console.log(`3.Element ${i}:,`,nombreActuel)
+
         if (compteur [nombreActuel]){
             compteur [nombreActuel] += 1
             
@@ -25,15 +30,40 @@ function searchTruder(nombres) {
         }
     }
     console.log(compteur)
-
-    for( let nombre in compteur) {
-        if (compteur[nombre]===1){
+    for (let nombre in compteur) {
+        console.log(`nombre: ${nombre}, typr: ${typeof nombre}, valeur: ${compteur[nombre]}`)
+        if (compteur[nombre] === 1) {
+            console.log("Trouvé! ", nombre)
             return Number[nombre]
         }
     }
     return null
 }
 
-console.log(searchTruder([1, 2, 3, 4, 5, 4, 3, 2, 1]))
+let resultat = searchTruder([1, 2, 3, 4, 5, 4, 3, 2, 1])
+console.log("resultat: ", resultat)
+
+/* 
+//Parsing 
+function getArguments() {
+    const args = process.argv.slice(2)
+    return args
+}
+
+//Gestion d'erreur 
+function isValidArguments(args) {
+    if (args.length !==) {
+        console.error("Erreur vous devez saisir ")
+        return false 
+    }
+
+    return true
+}
+
+//Resolution 
+function getsearchTruder() {
+    let args = getArguments()
 
 
+
+} */
