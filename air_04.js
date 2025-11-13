9d2964692c6d220189926e2a6c78acf048cf161f
anaky94 <anaky.44@gmail.com>
2025-11-13 10:30:01 +0100

    air

--- air_04.js
+++ air_04.js
@@ -13,6 +13,7 @@ function removeAdjacentDuplicate (storStrings) {
     if ( storStrings.length === 0 ){
         return ""
     }
+    
     let resultat = storStrings[0]
     
     for (let i = 1; i < storStrings.length; i++){
--- air_05.js
+++ air_05.js
@@ -1,19 +1,65 @@
 /* import re
-"""
-Un seul à la fois
-    Créez un programme qui affiche une chaîne de caractères en évitant les caractères identiques adjacents.
+Créez un programme qui est capable de reconnaître et de faire une opération (addition ou soustraction) sur chaque entier d’une liste.
+
 
 Exemples d’utilisation :
-$> python exo.py “Hello milady,   bien ou quoi ??”
-Helo milady, bien ou quoi ?
+$> ruby exo.rb 1 2 3 4 5 “+2”
+3 4 5 6 7
+
+$> ruby exo.rb 10 11 12 20 “-5”
+5 6 7 15
 
 
+L’opération à appliquer sera toujours le dernier élément.
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
  */
 
+//lobjecif est de fair une opération mathématique avec chaque entité d'une liste 
+//L'operation ce trouve dans le dernier élement en chaine de caractère 
+
+/* Pseudo code
+    
+ */
+
+function displayOfStrings() {
+    let tableResult = []
+    let list  = [10,11,12,20]
+    let numbercalculator = [+2]
+
+    for(element of list){
+        console.log(element + numbercalculator[0])
+    }
+    return tableResult
+}
 
 
+
+//Parsing
+function getArguments() {
+    const args = process.argv.slice(2)
+    return args
+}
+
+//gestion d'erreur 
+function isValidArguments(args) {
+    if (args.length =! 2 ) {
+        console.error("Erreur")
+        return false
+    }
+    return true
+}
+
+//Resilution 
+function getDisplayOfStrings (){
+    let args = getArguments()
+
+    if(!isValidArguments(args)) return
+    
+
+    const list = args[0]
+    const numbercalculator= args[1]
+}
 
 
 
